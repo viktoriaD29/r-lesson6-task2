48f7c37e00b8caa5600a36777810b9cc31eaa7c6
Viktoria <doduchenko29@gmail.com>
2021-07-11 21:45:48 +0300

    add

--- src/Mailbox.jsx
+++ src/Mailbox.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
 
-const Mailbox = ({ undeadMessages }) => {
+const Mailbox = ({ unreadMessages }) => {
   return (
     <div className="mailbox">
       <span className="mailbox__text">Messages</span>
-      {undeadMessages.length > 0 && (
-        <span className="mailbox__count">{undeadMessages.length}</span>
+      {unreadMessages.length > 0 && (
+        <span className="mailbox__count">{unreadMessages.length}</span>
       )}
     </div>
   );
